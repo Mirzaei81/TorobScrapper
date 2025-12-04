d75beab2f8b0b39847d422777c3028b7b5beea08
pc gamer <aam.mirzaei@gmail.com>
2025-12-04 16:29:57 +0330

    fix brandings

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,4 +1,6 @@
 import undetected_chromedriver as uc
+
+
 from json import loads
 import os
 import pandas
@@ -26,23 +28,28 @@ def get_sku_name(id):
     'Cookie': 'pxcelPage_c01002=1'
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = session.get(url, headers=headers, data=payload)
 
     return response.json()
 
-import chromedriver_autoinstaller
+# import chromedriver_autoinstaller
 # from pyvirtualdisplay import Display
 # display = Display(visible=0, size=(800, 800))  
 # display.start()
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+# chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
                                       # and if it doesn't exist, download it automatically,
                                       # then add chromedriver to path
 
 WOOCOMERCE_KEY=os.getenv("WOOCOMERCE_KEY")
 WOOCOMERCE_SECRET=os.getenv("WOOCOMERCE_SECRET")
-res = requests.get("https://zardaan.com/wp-json/torob/v1/listProds")
+session = requests.Session()
+
+
+
+res = session.get("https://zardaan.com/wp-json/torob/v1/listProds")
 
 chrome_options = webdriver.ChromeOptions()
+
 # # Add your options as needed
 options = [
 # Define window size here
@@ -63,23 +70,22 @@ for prod in data["response"]:
     key = prod["meta_value"]
     driver.get(f"https://api.torob.com/v4/base-product/sellers/?prk={key}")
     zardanProd = get_sku_name(prod["post_id"])
-    if zardanProd["brands"] and zardanProd["brands"][0]["id"]==7328:
-        elm = driver.find_element(uc.By.XPATH,value="/html/body/pre")
+    elm = driver.find_element(uc.By.XPATH,value="/html/body/pre")
 
-        data = loads(elm.text)
-        for i,res in enumerate(data["results"]):
-            if  not res["availability"]:
-                break
-            if res["price"]!=0:
-                productData["ids"].append(zardanProd["id"])
-                productData["names"].append(zardanProd["slug"])
-                productData["sku"].append(zardanProd["sku"])
-                productData["prices"].append(res["price"])
-                productData["shops"].append(res["shop_name"])
-                productData["urls"].append(zardanProd["permalink"])
-                if res["shop_name2"]=="تهران":
-                    productData["locs"].append("T")
-                else:
-                    productData["locs"].append("nT")
+    data = loads(elm.text)
+    for i,res in enumerate(data["results"]):
+        if  not res["availability"]:
+            break
+        if res["price"]!=0:
+            productData["ids"].append(zardanProd["id"])
+            productData["names"].append(zardanProd["slug"])
+            productData["sku"].append(zardanProd["sku"])
+            productData["prices"].append(res["price"])
+            productData["shops"].append(res["shop_name"])
+            productData["urls"].append(zardanProd["permalink"])
+            if res["shop_name2"]=="تهران":
+                productData["locs"].append("T")
+            else:
+                productData["locs"].append("nT")
 df = pandas.DataFrame(productData)
 df.to_csv("product_data.csv",encoding="utf-8-sig")
